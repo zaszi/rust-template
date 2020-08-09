245d0724e6ca829ad6f8aa4c63691d5248de3237
zaszi <admin@zaszi.net>
2020-08-09 18:03:32 +0200

    Add minimal WebAssembly-ready Rust source

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+const _VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[wasm_bindgen]
+/// Run our program, put all program logic here or in subsequent modules.
+pub fn run() {
+    set_panic_hook();
+    // Program logic here
+    println!("Hello, world");
+}
+
+/// Improve error messages in the browser when running as WebAssembly.
+/// For more details see
+/// https://github.com/rustwasm/console_error_panic_hook#readme
+fn set_panic_hook() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+}
